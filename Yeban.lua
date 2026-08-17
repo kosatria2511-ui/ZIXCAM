78f7413d3155c5bb6c4b6d1c3dd3b0e8a6ad38c5
Zixster <kosatria2511@gmail.com>
2026-08-17 12:56:44 +0800

    Create Yeban.lua with configuration settings
    Added configuration settings for various features including AntiSlow, DBDMedkit, Hooks, ESP, and AutoDagger.

---
+++ Yeban.lua
@@ -0,0 +1,105 @@
+{
+  "MenuKey": {
+    "__type": "Enum",
+    "name": "One",
+    "enum": "KeyCode"
+  },
+  "Config": {
+    "AntiSlow": {
+      "Enabled": true,
+      "Mode": "Toggle"
+    },
+    "DBDMedkit": {
+      "Enabled": true
+    },
+    "Hooks": {
+      "Enabled": false,
+      "Type": "AlwaysOnTop",
+      "OutlineTransparency": 0,
+      "Distance": 200,
+      "FillTransparency": 0.9,
+      "ShowName": false,
+      "OutlineColor": {
+        "b": 1,
+        "__type": "Color3",
+        "g": 0.3921568691730499,
+        "r": 1
+      },
+      "ShowDistance": false,
+      "FillColor": {
+        "b": 1,
+        "__type": "Color3",
+        "g": 0.3921568691730499,
+        "r": 1
+      },
+      "TextOutlineColor": {
+        "b": 0,
+        "__type": "Color3",
+        "g": 0,
+        "r": 0
+      },
+      "TextColor": {
+        "b": 1,
+        "__type": "Color3",
+        "g": 1,
+        "r": 1
+      }
+    },
+    "ESP": {
+      "Size": 14,
+      "Font": {
+        "__type": "Enum",
+        "name": "Jura",
+        "enum": "Font"
+      },
+      "PositionY": -4,
+      "BlackoutDuration": 0.3
+    },
+    "UnhookYourself": {
+      "Enabled": true,
+      "Bind": {
+        "__type": "Enum",
+        "name": "RightControl",
+        "enum": "KeyCode"
+      }
+    },
+    "AutoDagger": {
+      "Enabled": false,
+      "BindMode": "Toggle",
+      "Bind": {
+        "__type": "Enum",
+        "name": "Three",
+        "enum": "KeyCode"
+      },
+      "ShowParryRange": false,
+      "Distance": 14,
+      "Angle": 98,
+      "ShowParryAngle": false,
+      "RequirementsColor": {
+        "b": 0,
+        "__type": "Color3",
+        "g": 0,
+        "r": 0
+      }
+    },
+    ...
+  },
+  "BindListPos": {
+    "Y": 294,
+    "X": 1732
+  },
+  "Theme": {
+    "Accent": {
+      "b": 1,
+      "__type": "Color3",
+      "g": 1,
+      "r": 1
+    },
+    "AccentText": {
+      "b": 0.0784313753247261,
+      "__type": "Color3",
+      "g": 0.0784313753247261,
+      "r": 0.0784313753247261
+    }
+  }
+}
